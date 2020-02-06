39fe8c3b5d2bac8501a27743a05e8e34a94ea60f
James Jensen <jnjensen@live.com>
2020-02-06 03:05:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === ""){
+  if (typeof(cardNumber) === undefined){
     return "Sorry, we don't have a credit card on file for you."
   }
 }
