02a4bbf0772fc9cc9f5d7c44bce2a4d8eb7018fb
James Jensen <jnjensen@live.com>
2020-02-06 03:01:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
-    if (Object.keys(cart[i]) === item){
-      cart = [...cart.slice(0,i),...cart.slice(i+1)]
-      return cart
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1); //removes 1 item at index i
+      return cart;
     }
   }
   return "That item is not in your cart."
