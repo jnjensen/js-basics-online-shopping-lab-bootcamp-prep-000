30ebc0579fc1479a0188ff197beeed08f34f2c7c
James Jensen <jnjensen@live.com>
2020-02-06 02:55:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if Object.keys(cart[i]) === item){
-      delete cart[i]
+      cart = 
       return cart
     }else{
     continue}
